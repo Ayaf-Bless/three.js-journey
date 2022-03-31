f8ac8241071c744e96c1248632fd4c2a9e650a63
Bless Ambele <blessambel1@gmail.com>
2022-03-31 09:01:57 -0930

    create a basic scene,objects and render them

--- 01 basic scene/script.js
+++ 01 basic scene/script.js
@@ -1,28 +1,24 @@
-const canvas = document.querySelector(".wbgl");
-/*Scene*/
+const wbgl = document.querySelector(".wbgl");
 const scene = new THREE.Scene();
-
-/*creating a cube*/
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-//add the cube to the scene
-scene.add(mesh);
-
 const sizes = {
   width: 800,
   height: 600,
 };
 
-/*Camera*/
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.x = 1;
+camera.position.x = 2;
 scene.add(camera);
 
-/*Renderer*/
 const renderer = new THREE.WebGLRenderer({
-  canvas,
+  canvas: wbgl,
 });
 renderer.setSize(sizes.width, sizes.height);
+
 renderer.render(scene, camera);
